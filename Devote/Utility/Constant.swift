e08651d4705d488be34bea023bf81df1262c2268
NowFloats <prathap.reddy@nowfloats.com>
2023-09-22 18:28:57 +0530

    Prep for Core Data

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Prathap Reddy on 22/09/23.
+//
+
+import SwiftUI
+
+// MARK: - FORMATTER
+
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
