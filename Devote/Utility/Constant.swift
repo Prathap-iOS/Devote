0aabcaee9d1890d0bd2b16c7889535cab291d8cf
NowFloats <prathap.reddy@nowfloats.com>
2023-09-22 19:18:40 +0530

    Backgroud gradient and image added to content view

--- Devote/Utility/Constant.swift
+++ Devote/Utility/Constant.swift
@@ -18,4 +18,8 @@ private let itemFormatter: DateFormatter = {
 
 // MARK: - UI
 
+var backgroundGradient: LinearGradient {
+    return LinearGradient(gradient: Gradient(colors: [.pink, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+}
+
 // MARK: - UX
